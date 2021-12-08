9dafc64574fcf489df01a1aeb3003b26e0f0f6b6
Chinedu <chineduezeh19@gmail.com>
2021-12-08 06:25:28 +0100

    learnt how to write MDX and render it's content on web page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,27 +1,35 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 const BlogPage = ({data}) => {
     return (
         <Layout pageTitle="My blog page">
             <p>My blog post will go here</p>
-            <ul>
                 {
-                    data.allFile.nodes.map(node =>(
-                        <li key={node.name}>{node.name}</li>
+                    data.allMdx.nodes.map(node =>(
+                        <article key={node.name}>
+                        <h2>{node.frontmatter.title}</h2>
+                        <p>{node.frontmatter.date}</p>
+                        <MDXRenderer>{node.body}</MDXRenderer>
+                        </article>
                     ))
                 }
-            </ul>
         </Layout>
     )
 }
 
 export const query = graphql`
 query {
-    allFile {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
       nodes {
-        name
+        frontmatter {
+          title
+          date
+        }
+        id
+        body
       }
     }
   }
