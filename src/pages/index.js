556b85ca62fabd02f9df962256ac31dafe866f1e
Chinedu <chineduezeh19@gmail.com>
2021-12-08 04:45:43 +0100

    using gatsby plugin and static image component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
+import { StaticImage } from 'gatsby-plugin-image'
 import React from 'react'
 import Layout from '../components/layout'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following gatsby Docs. Wow I'm learing from documentation</p>
+      <StaticImage
+      src="https://ateenprogrammer.com/wp-content/uploads/2021/12/photo1638584119-e1638584296462-768x1076.jpeg"
+      alt="A nice image of chinedu"
+      />
     </Layout>
   )
 }
