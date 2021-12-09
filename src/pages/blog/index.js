3d6397b0a3afbebab2455109b540ce8ea31dc739
Chinedu <chineduezeh19@gmail.com>
2021-12-09 05:18:27 +0100

    Learnt how to create dynamic pages and rendering MDX body

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-import Layout from '../components/layout'
+import { graphql, Link } from 'gatsby'
+import Layout from '../../components/layout'
 
 const BlogPage = ({data}) => {
     return (
@@ -10,9 +9,12 @@ const BlogPage = ({data}) => {
                 {
                     data.allMdx.nodes.map(node =>(
                         <article key={node.name}>
-                        <h2>{node.frontmatter.title}</h2>
+                        <h2>
+                          <Link to={`/blog/${node.slug}`}>
+                          {node.frontmatter.title}
+                          </Link>
+                        </h2>
                         <p>{node.frontmatter.date}</p>
-                        <MDXRenderer>{node.body}</MDXRenderer>
                         </article>
                     ))
                 }
@@ -29,7 +31,7 @@ query {
           date
         }
         id
-        body
+        slug
       }
     }
   }
